e2f5ef353a3f5c271b69aef2c902942e35ccba54
Kasey Culmback <kasey.culmback@gmail.com>
2023-03-19 21:04:43 -0700

    feat: add search

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,36 @@
 import { CocktailsList } from '@/components'
 import { trpc } from '@/utils/trpc'
-import { Container, Heading, Stack } from '@chakra-ui/react'
+import { Container, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import { useMiniSearch } from 'react-minisearch'
 
 export default function Home() {
   const cocktails = trpc.getAllCocktails.useQuery({ filter: 'available' })
+
+  const { removeAll, addAll, search, searchResults } = useMiniSearch(cocktails.data ?? [], {
+    fields: ['label', 'description'],
+    searchOptions: {
+      fuzzy: true,
+      prefix: true,
+    },
+    idField: 'id',
+  })
+
+  useEffect(() => {
+    removeAll()
+    addAll(cocktails.data ?? [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cocktails])
+
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    if (!!query) {
+      search(query)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
 
   return (
     <>
@@ -18,7 +44,18 @@ export default function Home() {
             <Heading as="h1" size="lg">
               Available Cocktails
             </Heading>
-            <CocktailsList cocktails={cocktails.data ?? []} />
+
+            <FormControl>
+              <FormLabel htmlFor="query">Search Cocktails</FormLabel>
+              <Input
+                name="query"
+                placeholder="Whiskey..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </FormControl>
+
+            <CocktailsList cocktails={(!!query ? searchResults : cocktails.data) ?? []} />
           </Stack>
         </Container>
       </main>
