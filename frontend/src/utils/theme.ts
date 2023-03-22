1e84014791180e25409e63d499069d14063b2710
Kasey Culmback <kasey.culmback@gmail.com>
2023-03-22 12:43:54 -0700

    fix: update theme

--- frontend/src/utils/theme.ts
+++ frontend/src/utils/theme.ts
@@ -1,31 +1,10 @@
 import { extendTheme, theme as baseTheme } from '@chakra-ui/react'
-import { listAnatomy as parts } from '@chakra-ui/anatomy'
-import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/styled-system'
-
-const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpers(parts.keys)
-
-const baseStyleContainer = defineStyle({
-  pl: 6,
-})
-
-const baseStyle = definePartsStyle({
-  container: baseStyleContainer,
-})
-
-const listTheme = defineMultiStyleConfig({
-  baseStyle,
-})
 
 export const theme = extendTheme(baseTheme, {
   styles: {
     global: {
       'html, body': { bg: 'gray.50', minH: '100vh' },
-      'ul, ol': {
-        pl: 6,
-      },
+      body: { pb: 8 },
     },
-  },
-  components: {
-    List: listTheme,
   },
 })
