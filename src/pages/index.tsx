ae62c3134adb91e8afa5db463a7bf091d1cf76b3
Kasey Culmback <kasey.culmback@gmail.com>
2023-11-17 23:45:47 -0800

    fix: if this works...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
+import { CocktailsList, SearchCocktails, useSearchCocktails } from '@/components'
 import { trpc } from '@/utils/trpc'
-import { Container, Heading, Stack, Text } from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, Container, Heading, Skeleton, Stack } from '@chakra-ui/react'
+import { range } from 'lodash-es'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 
 export default function Home() {
   const cocktailsQuery = trpc.getAllCocktails.useQuery({ filter: 'inStock' })
 
-  // eslint-disable-next-line no-console
-  console.log(cocktailsQuery.data)
+  const { cocktails, query, setQuery, searchResults, tag, setTag, tags } = useSearchCocktails({
+    cocktails: cocktailsQuery.data,
+  })
 
-  // const { cocktails, query, setQuery, searchResults, tag, setTag, tags } = useSearchCocktails({
-  //   cocktails: cocktailsQuery.data,
-  // })
+  const { status } = useSession()
 
-  // const { status } = useSession()
-
-  // const isLoggedIn = status === 'authenticated'
+  const isLoggedIn = status === 'authenticated'
 
   return (
     <>
@@ -29,15 +29,15 @@ export default function Home() {
               Available Cocktails
             </Heading>
 
-            {/* <SearchCocktails
+            <SearchCocktails
               query={query}
               setQuery={setQuery}
               setTag={setTag}
               tag={tag}
               tags={tags}
-            /> */}
+            />
 
-            {/* {cocktailsQuery.isLoading ? (
+            {cocktailsQuery.isLoading ? (
               range(0, 3).map((i) => <Skeleton key={i} borderRadius="md" h="44" />)
             ) : cocktailsQuery.isError ? (
               <Alert status="error">
@@ -54,8 +54,7 @@ export default function Home() {
                 cocktails={(!!query ? searchResults : cocktails) ?? []}
                 includeActions={isLoggedIn}
               />
-            )} */}
-            <Text>Test</Text>
+            )}
           </Stack>
         </Container>
       </main>
